14b448833c94b924b4611bdefed08a2e18e1cf9e
Ryan Peach <ryan.peach@outlook.com>
2015-11-20 14:59:06 -0600

    Added all process methods including automatic, untested.

--- capture.hpp
+++ capture.hpp
@@ -23,6 +23,7 @@ class Capture {
     Fps *fps;              // Just a temp variable to hold the Fp's from the most recent frame
     Point *ref;            // The reference corner's location
     cnt *border;           // The border of the scan
+    vector<cnt> *rects;    // Rectangles identified in the image
 
     int angleTol;          // The angle tolerance app-wide
     int distTol;           // The distance tolerance for small polygons (like focus pointss)
@@ -34,13 +35,14 @@ class Capture {
     //Import Parameters
     int etol1, etol2, eSize;
 
-    enum Method {fpcorners, strongborder} sel;
+    enum Method {fpcorners, strongborder, regular, automatic} sel;
 
     // Preprocessing allows each process to share data,
     // so that nothing is calculated twice for the same image.
     Mat* getEdges();
     Cnts* getPolys();
     Fps* getFps();
+    vector<cnt>* getRects();
 
     Point* getRef();
     cnt* getBorder();
@@ -61,7 +63,7 @@ class Capture {
 
     Capture (int angleTol = 10, int distTol = 5, int polyTol = 5,
              double aspectRatio = 8.5/11.0, double sizeRatio = .5, double ratioTol = .1,
-             int etol1 = 100, int etol2 = 200, int eSize = 3, Method sel = fpcorners):
+             int etol1 = 100, int etol2 = 200, int eSize = 3, Method sel = automatic):
                 angleTol(angleTol), distTol(distTol), polyTol(polyTol),
                 aspectRatio(aspectRatio), sizeRatio(sizeRatio), ratioTol(ratioTol),
                 etol1(etol1), etol2(etol2), eSize(eSize), sel(sel)
