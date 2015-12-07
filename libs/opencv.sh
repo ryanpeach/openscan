9a1f7000532d7b5f11ad98137fd693be795cb544
Ryan Peach <ryan.peach@outlook.com>
2015-12-07 13:55:57 -0600

    Getting ready to use with JNI

--- libs/opencv.sh
+++ libs/opencv.sh
@@ -7,10 +7,10 @@ sudo apt-get -qq remove ffmpeg x264 libx264-dev
 echo "Installing Dependenices"
 sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg cmake qt5-default checkinstall
 echo "Downloading OpenCV" $version
-wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
+cd ..
+git submodule update --init --recursive
 echo "Installing OpenCV" $version
-unzip OpenCV-$version.zip
-cd opencv-$version
+cd ./libs/opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
